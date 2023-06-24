0ff6d8e6fabb2958d8fe8e9f964f9e89acf28108
Prakash singh <pa7846665@gmail.com>
2023-06-25 01:07:41 +0530

    test case 1 passed

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Hero_section from "./Hero_section";
+import Footer from "./Footer";
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Hero_section />
+      <Footer />
+    </>
+  )
+}
+
+export default App
